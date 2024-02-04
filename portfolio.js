0337285274861850c7124bac6bf46fffde24aaf4
Simon <sila0002@stud.kea.dk>
2024-02-05 00:38:24 +0100

    Første upload af site

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+  console.log("sidenVises");
+}
+
+function scrollToTop() {
+  console.log("scrollToTop");
+  window.scrollTo(0, 0);
+}
