4f54da6612742c4f7514d6da8f664758df77380d
Simon <sila0002@stud.kea.dk>
2024-03-04 07:43:09 +0100

    forklarer JS

--- portfolio.js
+++ portfolio.js
@@ -1,16 +1,26 @@
 window.addEventListener("load", sidenVises);
 
+//Jeg starter ud med en console.log for at være sikker på at der er forbindelse til min Javascript.
+
 function sidenVises() {
   console.log("sidenVises");
 }
+
+// Her rammer jeg et element inde i min HTML ved at sigte efter dens ID.
+// Jeg tilføjer class'en "open", for at åbne op for min burger menu.
+// Den er kædet sammen med min html, hvor jeg har en "onclick()" sat på min menu.
 
 function openNav() {
   document.getElementById("minSidebar").classList.add("open");
 }
 
+// Jeg fjerner class'en "open", for at åbne op for min burger menu.
+
 function closeNav() {
   document.getElementById("minSidebar").classList.remove("open");
 }
+
+// scrollTo scroller efter kordinator, hvor at den lander tilbage på 0 (starten) både horisontalt og vertikalt.
 
 function scrollToTop() {
   console.log("scrollToTop");
