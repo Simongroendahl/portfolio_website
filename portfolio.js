3ee4bb92b7a2bccf1c255517fe97101d122166b8
Simon <sila0002@stud.kea.dk>
2024-03-04 07:23:21 +0100

    final update

--- portfolio.js
+++ portfolio.js
@@ -4,6 +4,14 @@ function sidenVises() {
   console.log("sidenVises");
 }
 
+function openNav() {
+  document.getElementById("minSidebar").classList.add("open");
+}
+
+function closeNav() {
+  document.getElementById("minSidebar").classList.remove("open");
+}
+
 function scrollToTop() {
   console.log("scrollToTop");
   window.scrollTo(0, 0);
